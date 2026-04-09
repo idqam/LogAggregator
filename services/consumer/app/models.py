d8bb3425c6aff2d19dc1d346f8b12da85fe4de64
idqam <owenjave5@gmail.com>
2026-04-08 21:00:30 -0500

    created inital config class

--- services/consumer/app/models.py
+++ services/consumer/app/models.py
@@ -1,1 +1,27 @@
-import pydantic 
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class KafkaConfig(BaseModel):
+    broker: str
+    topic: str
+    group_id: str
+
+
+class PostgresConfig(BaseModel):
+    url: str
+
+
+class PrometheusConfig(BaseModel):
+    port: int = 8001
+
+
+class Settings(BaseSettings):
+    kafka: KafkaConfig
+    postgres: PostgresConfig
+    prometheus: PrometheusConfig
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_nested_delimiter="__"
+    )
